8a824a7866eeaae6b13544c3779586cb5cc4950e
nbdunks <thenicoleduncan@gmail.com>
2019-09-11 21:00:31 -0700

    reformatted screen design with css, added new gems, included a new game button, a score button, a you in and a you lose to pop up

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,10 @@
+
 var targetNumber = 53;
+var youWin = ("You win!! 💎");
+var youLose = ("You lose!! 👎");
 
 $("#number-to-guess").text(targetNumber);
+
 
 var counter = 0;
 
@@ -12,7 +16,7 @@ for (var i = 0; i < numberOptions.length; i++) {
 
     imageCrystal.addClass("crystal-image");
 
-    imageCrystal.attr("src", "https://ion.r2net.com/sgmdirect/photoID/2187280/diamond/21600/diamond--3.19-Carat--_4_first_.jpg");
+    imageCrystal.attr("src", "./assets/images/pink-gem.JPG");
 
     imageCrystal.attr("data-crystalvalue", numberOptions[i]);
 
@@ -20,27 +24,23 @@ for (var i = 0; i < numberOptions.length; i++) {
 }
 
 $(".crystal-image").on("click", function() {
-
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
+    var audio = $("#mySoundClip")[0];
+    audio.play();
 
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
     counter += crystalValue;
 
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    alert("New score: " + counter);
+    // alert("New score: " + counter);
+    $("#new-score").text(counter);
 
     if (counter === targetNumber) {
-      alert("You win!");
+        $("#you-win").text(youWin);
+        // document.getElementById("you-win").style.border = " 17px dotted rgba(248,255,102,0.97)";
     }
 
     else if (counter >= targetNumber) {
-      alert("You lose!!");
+        $("#you-lose").text(youLose);
     }
 
   });
